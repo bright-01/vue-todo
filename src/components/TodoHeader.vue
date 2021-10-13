893e8cba08f8620fc5842556634b27acf2b4c780
bright-01 <cheonmyeongwon@gmail.com>
2021-10-13 10:40:54 +0900

    todo list 기본 기능 및 ui

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
-      header
-  </div>
+  <header>
+      <h1>TODO it!</h1>
+  </header>
 </template>
 
 <script>
@@ -10,6 +10,10 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style scoped>/* scoped 이 옵션은 css가 밑으로 상속되는걸 방지하고 이 컴포넌트에서만 적용된다*/
+  h1{
+    color: #2F3852;
+    font-weight: 900;
+    margin: 2.5rem 0 1.5rem;
+  }
 </style>
--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,15 +1,63 @@
 <template>
-  <div>
-    input
+  <div class="inputBox shadow">
+    <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo"><!-- v-model >> input box의 값을 컴포넌트의 데이터의 바로 바인딩한다   -->
+<!--    <button v-on:click="addTodo">add</button>-->
+    <span class ="addContainer" v-on:click="addTodo">
+      <i class="fas fa-plus addBtn"></i>
+    </span>
   </div>
 </template>
 
 <script>
 export default {
-  name: "TodoInput"
+  name: "TodoInput",
+  data: function(){
+    return {
+      newTodoItem : ""
+    }
+  },
+  methods: {
+    addTodo : function(){
+      console.log(this.newTodoItem);
+      // 저장하고
+      if(this.newTodoItem !== ''){
+        var obj = { completed: false, item: this.newTodoItem}
+        localStorage.setItem(this.newTodoItem, JSON.stringify(obj))
+        this.clearInput();
+      }
+    },
+    clearInput: function(){
+      this.newTodoItem = "";
+    }
+  }
 }
 </script>
 
 <style scoped>
+  input:focus{
+    outline: none;
+  }
+  .inputBox{
+    background: white;
+    height: 50px;
+    line-height: 50px;
+    border-radius:5px;
+  }
+  .inputBox input{
+    border-style: none;
+    font-size: 0.9rem;
+  }
+  .addContainer{
+    float: right;
+    background: linear-gradient(to right, #6478FB, #8763FB);
+    display:block;
+    width:3rem;
+    border-radius: 0 5px 5px 0;
+  }
+  .addBtn{
+    color: white;
+    vertical-align: middle;
+  }
+
 
 </style>
